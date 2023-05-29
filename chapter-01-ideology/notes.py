0a57ce0a72ee3ca8945eb6078edcd8418528dfdd
Austin Do <austinndo@gmail.com>
2023-05-29 12:38:00 -0700

    Trying out docstring to comment out page number and title

--- chapter-01-ideology/notes.py
+++ chapter-01-ideology/notes.py
@@ -1,5 +1,9 @@
-##### Chapter 1 Notes ######
-# pg. 13 Test Code - Unit Testing
+######################### Chapter 1 Notes ##########################
+
+"""
+pg. 13 Test Code - Unit Testing 
+
+"""
 
 EPS = 0.00001
 # a small number to use when comparing floating-point values
@@ -13,8 +17,9 @@ def add(x, y):
 def test_add():
     assert (add(2, 3) == 5)
     assert (add(-2, 3) == 1)
-    assert (add(-1, -1) == 2)
+    assert (add(-1, -1) == -2)
     assert (abs(add(2.4, 0.1) - 2.5) < EPS)
+    print("All tests passed")
 
 
 test_add()
