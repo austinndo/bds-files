772faf8a0f164643191cc5a84fc801d84b257a25
Austin Do <austinndo@gmail.com>
2023-05-29 12:28:45 -0700

    Forked bds-files and adding notes for each chapter as I read through. Trying the unit test featured on pg. 13

---
+++ chapter-01-ideology/notes.py
@@ -0,0 +1,20 @@
+##### Chapter 1 Notes ######
+# pg. 13 Test Code - Unit Testing
+
+EPS = 0.00001
+# a small number to use when comparing floating-point values
+
+
+def add(x, y):
+    print(x+y)
+    return x + y
+
+
+def test_add(x, y):
+    assert (add(2, 3) == 5)
+    assert (add(-2, 3) == 1)
+    assert (add(-1, -1) == 2)
+    assert (abs(add(2.4, 0.1) - 2.5) < EPS)
+
+
+test_add()
