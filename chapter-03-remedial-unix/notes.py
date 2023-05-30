455be062ba57915691d5a16844a85c8c53a4ef62
Austin Do <austinndo@gmail.com>
2023-05-30 12:34:23 -0700

    Practiced cat command in terminal

---
+++ chapter-03-remedial-unix/notes.py
@@ -0,0 +1,7 @@
+######################### Chapter 3 Notes ##########################
+
+"""
+'cat' - Concatenate; to print a file's contents to standard out (kind of like print() in python)
+    it also allows one to print multiple files' contents to the standard output stream
+
+"""
