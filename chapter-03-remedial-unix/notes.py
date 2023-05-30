5e971c04a2839c292ab97c5f2feb67dffa074586
Austin Do <austinndo@gmail.com>
2023-05-30 12:39:29 -0700

    Created zea-proteins file by performing command in terminal to read existing files and redirect the standard output to this new file

--- chapter-03-remedial-unix/notes.py
+++ chapter-03-remedial-unix/notes.py
@@ -3,5 +3,8 @@
 """
 'cat' - Concatenate; to print a file's contents to standard out (kind of like print() in python)
     it also allows one to print multiple files' contents to the standard output stream
+'>' - this operator redirects standard output to a file. this will overwrite existing contents! if file doesn't already exist, it will be created
+'>>' - this operator appends to the file. if file doesn't already exist, it will be created
 
+ran the following in the terminal 'cat tb1-protein.fasta tga1-protein.fasta > zea-proteins.fasta' and created the zea-proteins.fasta file
 """
