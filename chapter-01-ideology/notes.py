f48c0bdd8478894b2e5c9295f00b93281c813b47
Austin Do <austinndo@gmail.com>
2023-05-30 12:00:46 -0700

    Update

--- chapter-01-ideology/notes.py
+++ chapter-01-ideology/notes.py
@@ -3,6 +3,11 @@
 """
 pg. 13 Test Code - Unit Testing 
 
+Creating unit tests can be costly. Consider these three points each time you code:
+
+1. How  many times is this code called by other code?
+2. If this code WERE wrong, how detrimental to the results would it be?
+3. How noticeable would an error be if it occurred?
 """
 
 EPS = 0.00001
